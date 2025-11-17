42ab008faf7e8a2e9934b5926779cb693aae7093
Boluex <awomewekolawole1.com>
2025-11-17 12:49:38 +0100

    monitoring with prometheus

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template
 from controllers.member_controller import members_blueprint
 from controllers.fitness_class_controller import fitness_classes_blueprint
 from controllers.booking_controller import bookings_blueprint
+from prometheus_flask_exporter import PrometheusMetrics
+
 app = Flask(__name__)
 
 app.register_blueprint(bookings_blueprint)
 app.register_blueprint(fitness_classes_blueprint)
 app.register_blueprint(members_blueprint)
-
+metrics = PrometheusMetrics(app)
 @app.route('/')
 def home():
     return render_template('index.html', current_page = 'Home', title='Home')
